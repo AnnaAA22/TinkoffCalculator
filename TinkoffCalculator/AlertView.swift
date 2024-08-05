e0b04dac07689ff7e1e6b98ae11115ead3469600
AnnaAA <a2nia@mail.ru>
2024-08-06 00:31:38 +0400

    Added animation

---
+++ TinkoffCalculator/AlertView.swift
@@ -0,0 +1,8 @@
+//
+//  AlertView.swift
+//  TinkoffCalculator
+//
+//  Created by Анна Алёшина on 27.07.2024.
+//
+
+import Foundation
---
+++ TinkoffCalculator/LongPressView.swift
@@ -0,0 +1,8 @@
+//
+//  LongPressView.swift
+//  TinkoffCalculator
+//
+//  Created by Анна Алёшина on 05.08.2024.
+//
+
+import Foundation
